946fdbdd040661737dc38f76368bae2d127a55b1
Duncan Robertson <duncan@kyan.com>
2022-09-07 11:49:53 +0100

    Add simple fuzzy match

--- helpers/builder.ts
+++ helpers/builder.ts
@@ -21,7 +21,14 @@ const Builder = (items: Item[], prefix: string, action: string) => {
     };
   };
 
-  const matches = (title: string) => title.includes(action);
+  // https://codereview.stackexchange.com/a/23905
+  const fuzzy_match = (str: string, pattern: string) => {
+    if (!pattern) return true;
+    pattern = pattern.replace(/ /g, '').split("").reduce(function (a, b) { return a + ".*" + b; });
+    return (new RegExp(pattern)).test(str);
+  };
+
+  const matches = (title: string) => fuzzy_match(title, action);
 
   return {
     addItem: async (item: ItemMeta) => {
