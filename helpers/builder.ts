3d8d2f67d4ac9db860aefa39e749856313eb5e48
Duncan Robertson <duncan@kyan.com>
2022-09-07 11:59:48 +0100

    Fix names and add tests

--- helpers/builder.ts
+++ helpers/builder.ts
@@ -21,20 +21,21 @@ const Builder = (items: Item[], prefix: string, action: string) => {
     };
   };
 
-  // https://codereview.stackexchange.com/a/23905
-  const fuzzy_match = (str: string, pattern: string) => {
-    if (!pattern) return true;
-    pattern = pattern.replace(/ /g, '').split("").reduce(function (a, b) { return a + ".*" + b; });
-    return (new RegExp(pattern)).test(str);
-  };
-
-  const matches = (title: string) => fuzzy_match(title, action);
-
   return {
     addItem: async (item: ItemMeta) => {
-      if (matches(item.title)) addListItem(await buildListItem(item));
+      if (matches(item.title, action)) addListItem(await buildListItem(item));
     },
   };
 };
 
+const matches = (title: string, action: string) => fuzzyMatch(title, action);
+
+// https://codereview.stackexchange.com/a/23905
+const fuzzyMatch = (str: string, pattern: string) => {
+  if (!pattern) return true;
+  pattern = pattern.replace(/ /g, '').split("").reduce(function (a, b) { return a + ".*" + b; });
+  return (new RegExp(pattern)).test(str);
+};
+
 export default Builder;
+export { fuzzyMatch }
