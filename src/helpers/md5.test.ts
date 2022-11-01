4ad1b959ac52bb517a98574c65bc80639c71e7ac
Duncan Robertson <duncan@kyan.com>
2022-11-01 19:18:07 +0000

    chore: fix formatting

--- src/helpers/md5.test.ts
+++ src/helpers/md5.test.ts
@@ -4,11 +4,11 @@ import { createMd5Hash } from "./md5.ts";
 Deno.test("#createMd5Hash", async () => {
   assertEquals(
     await createMd5Hash("duncan"),
-    "f4d677934c35431de0c814a1bdc9993c"
+    "f4d677934c35431de0c814a1bdc9993c",
   );
 
   assertEquals(
     await createMd5Hash("whomwah"),
-    "f20c1b3028aca7d8f28bc5ac0ab4067b"
+    "f20c1b3028aca7d8f28bc5ac0ab4067b",
   );
 });
